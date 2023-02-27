864067205caf53a9b98b8407c29fa336e1f81a98
Stella Oliveira <80723301+StellaCorreiaF@users.noreply.github.com>
2023-02-27 11:31:06 -0300

    att schema

--- todosRails/db/schema.rb
+++ todosRails/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_24_190359) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_27_140844) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -54,6 +54,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_24_190359) do
     t.text "descricao"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "vencimento"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
