a11a55798537de84ace583d7f0ac33ff1b2a5f05
Stella Oliveira <80723301+StellaCorreiaF@users.noreply.github.com>
2023-02-28 16:26:16 -0300

    add scope

--- todosRails/app/models/task.rb
+++ todosRails/app/models/task.rb
@@ -1,3 +1,6 @@
 class Task < ApplicationRecord
   has_rich_text :descricao
+  scope :completas, -> { where(executada: true)}
+  scope :incompletas, -> { where(executada: [false, nil])}
+  belongs_to :topico
 end
