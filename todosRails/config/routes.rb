6b9757f74d8ae79cd33ceb635ff9eedaec32d5e2
Stella Oliveira <80723301+StellaCorreiaF@users.noreply.github.com>
2023-03-02 18:37:38 -0300

    create new routes

--- todosRails/config/routes.rb
+++ todosRails/config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
   resources :tasks
-  root to: redirect("/tasks")
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :topicos
+  #resources substitui essas rotas abaixo - o mapeamento delas
+  # root to: redirect("/tasks")
+  # get "/topicos", to: "topicos#index"
+  # post "/topicos", to: "topicos#create"
+  # get "/topicos/new", to: "topicos#new", as: :new_topico
+  # get "/topicos/:id/edit", to: "topicos#edit", as: :edit_topico
+  # get "/topicos/:id", to: "topicos#show", as: :topico
+  # patch "topicos/:id", to: "topicos#update"
+  # put "topicos/:id", to: "topicos#update"
+  # delete "topicos/:id", to: "topicos#destroy"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
