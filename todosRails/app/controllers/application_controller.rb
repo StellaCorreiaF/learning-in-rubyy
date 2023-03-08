d2518028a896b14851defb78f2f8f93b371a59ff
Stella Oliveira <80723301+StellaCorreiaF@users.noreply.github.com>
2023-03-08 18:22:19 -0300

    padroniza before_action no controller da aplicação geral

--- todosRails/app/controllers/application_controller.rb
+++ todosRails/app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  layout :set_layout
+
+  private
+
+  def set_layout
+    current_user.present? ? "signed_in" : "application"
+  end
 end
